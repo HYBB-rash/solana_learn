873d84624c66cbc11a6a9f02884cb1a18934f354
Herman Hong <hyong_cs@outlook.com>
2024-06-22 22:02:22 +0800

    使用新api

--- check-balance.ts
+++ check-balance.ts
@@ -1,4 +1,4 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 const suppliedPublicKey = process.argv[2];
 if (!suppliedPublicKey) {
@@ -6,7 +6,7 @@ if (!suppliedPublicKey) {
 }
 
 const publicKey = new PublicKey(suppliedPublicKey);
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+const connection = new Connection(clusterApiUrl("devnet"));
 
 const balanceInLamports = await connection.getBalance(publicKey);
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
