6a101a3df22c384ceb0f5bc180d335faf8da6c09
Herman Hong <hyong_cs@outlook.com>
2024-06-22 16:22:38 +0800

    step2: 提供参数, 查询任意一个钱包的sol数量

--- check-balance.ts
+++ check-balance.ts
@@ -1,5 +1,11 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-const publicKey = new PublicKey("9Rxa2GA7cTT8o1PxTEK5rp17TASSWnL8VYgFmF5TkfZ1");
+
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+    throw new Error("Provide a public key to check the balance of!");
+}
+
+const publicKey = new PublicKey(suppliedPublicKey);
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
 const balanceInLamports = await connection.getBalance(publicKey);
